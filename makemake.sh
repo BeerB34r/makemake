dcbe048465837f09ae293ca26e2d3ee004a00ed8
Mats De Beer <mde-beer@student.codam.nl>
2025-07-17 21:01:44 +0200

    pluh

--- makemake.sh
+++ makemake.sh
@@ -24,67 +24,22 @@
 # instead of the standard dune related tidbits
 # dune and doom are currently the only ones i can think of funny things for :P
 
-echo "Bless the Maker and His water.
-Bless the coming and going of Him.
-May His passage cleanse the world.
-May He keep the world for His people.
-"
-maker_header="#--------------------------------------------------------------------------------#
-#                                                                                #
-#                .                                                               #
-#              *###. +..                                                         #
-#             .###%####*.-*...                                                   #
-#             -##%%%%*%#*%#  :                                                   #
-#             +#%%%=.*%%%%+%%....                                                #
-#            .#%%%%%#**%*%=%%%%....                                              #
-#            .###%%-%+*:%%%%%%%@  .*     .                                       #
-#            .%%%%%%%*@%#...*: #.  .=. #      -                                  #
-#       .    .#=%%%%%*%%@%*. =.     ..= @  +                                     #
-#      .*%#.. +%%@%@@@#@@.=:# .  :  * .@                                         #
-#       ..@#:.#%%%@@@@%@@:-:*.-.. ..  . :.                                       #
-#           #%*%%%=%%* +%%@+....# % - *  +        .                              #
-#            .#%#%:=%%=#*..+#: ....... :. ..                                     #
-#            =..%%%##*%.::..+*...  ..   .  .                                     #
-#               ..%%-@*-.:%@:.. .#.=.  . .. ..                                   #
-#                 . @@@=%%@:+.-*--:.:.+....+  :                                  #
-#       .        ... @%-%:#.%@..+..* %..=  ... .*@=:.                            #
-#                #  ....#@@%#*:.**.@@**..%--.  ..@...               .#....       #
-#              ....  . ...@@@.%.+%*=-#.:::+.  . .  %  +*..                       #
-#                    .   ...@@-%@-.#-.@..#:.... .   *..             :            #
-#                   =      ..#@#@@+:--@-@.::.%.  ...@ .. -                       #
-#                  ..      .  .%@@@@=%@@ #:.--+. -   .    .@.-=.        ..      .#
-#      ..       .    ...-+*#==--@@@@@:. @@:-%..#=.:.-..  .   :.        . :*%.    #
-#                                ..%@@@@.@:%#=+*@%..    .**.-. @. * ..... =.*..  #
-#                             .    . @@+.@.#@:+--..=.::.: :.:..--+=... ..........#
-#                      . .... ..... ..:@@@@*@..@....-*+:.+#@:@@ .... ....... ....#
-#              ....   ::::*++-:.=:    .::+@@@@%@%*.#-. -.@@  .  ....           ..#
-# **-....  .  .=#:+-..        .. ..  .... .#@@@%+=@-#@.                      ....#
-#.-.     ........=:..     .+.-#+*##+##++#%%%#@@@=-                               #
-#  .    .... . . . ..   . . .*-...#+==:=..:@#.                                  .#
-#   .#+.  . .++.     .:+:==:.. .:*##+=-@=..                                      #
-#       . .-=:..:-. ....  ....=*=--@%@.                                          #
-#    .  .   ...-%+=+%.#%%=::.=......                                             #
-#+-----:::..     ..........-@:@                                                  #
-#..        .. . .   ..+...%.                                                     #
-#:         ....+#-..                                                             #
-#... ......                                                                      #
-#..                                                                              #
-#.                                                                               #
-#                         Bless the Maker and His water.                         #
-#                       Bless the coming and going of Him.                       #
-#                       May His passage cleanse the world.                       #
-#                     May He keep the world for His people.                      #
-#--------------------------------------------------------------------------------#"
+if [ -e ${HOME}/.makemakerc ]; then
+	. ${HOME}/.makemakerc
+fi
 
-savefile="sand.trout"
+. utils/variables
+. utils/functions
+
+echo -n "${prelude}"
 
 # check if this is the initial run of makemake
 if [ -e ${savefile} ]; then
 	. ${savefile}
 else
-	echo "what is the name of the target binary? (default: \"`basename $(realpath .)`\")"
+	echo "what is the name of the target binary? (default: \"`default_targetbin`\")"
 	read targetbin
-	targetbin=${targetbin:=`basename $(realpath .)`}
+	targetbin=${targetbin:=`default_targetbin`}
 	echo "target binary shall be named ${targetbin}"
 	echo "
 will this project be using C or CPP?"
@@ -113,22 +68,22 @@ will this project be using C or CPP?"
 		esac
 	done
 	echo "
-what directory should intermediary binary files be stored? (default: \"bin/\")"
+what directory should intermediary binary files be stored? (default: \"`default_bindir`\")"
 	read bindir
-	bindir=${bindir:=bin/}
+	bindir=${bindir:=`default_bindir`}
 	echo "intermediary binaries stored in: ${bindir}"
 	echo "
-what directory should generated dependency files be stored (default: \"dep/\")"
+what directory should generated dependency files be stored (default: \"`default_depdir`\")"
 	read depdir
-	depdir=${depdir:=dep/}
+	depdir=${depdir:=`default_depdir`}
 	echo "dependency files stored in: ${depdir}"
 	echo "
 will this project be using local libraries, such as minilibx? (y/n)"
 	read reply
 	if [ "${reply}" = "y" -o -z ${reply} ]; then
-		echo "in what directory are the libraries stored? (default: \"lib/\")"
+		echo "in what directory are the libraries stored? (default: \"`default_libdir`\")"
 		read libdir
-		libdir=${libdir:=lib/}
+		libdir=${libdir:=`default_libdir`}
 		echo "libraries stored in: ${libdir}"
 		echo "NOTE: this makefile generator assumes that each library has its own directory, and outputs an archive file corresponding to its directory name, i.e: ./lib/libfoo => ./lib/libfoo/libfoo.a"
 		echo "if this is not the case for your project, you must manually change this yourself"
@@ -143,162 +98,11 @@ will this project be using local libraries, such as minilibx? (y/n)"
 	fi
 fi
 
-PS3="Please select a number: "
-# ask for user input on everything not provided by any previous runthrough of
-# makemake
-if [ -z ${directories} ]; then
-	directories=(`find . -type d -not -path '*/.*' -printf "%P/\n"` "quit")
-fi
-get_srcdirs() {
-	if [ -z ${bindir} ]; then bindir="bin/"; fi
-	if [ -z ${srcdirs} ]; then
-		echo "
-which of the following directories contain your source files? if source files are in nested directories, please submit all relevant directories"
-		select option in "${directories[@]:1}"
-		do
-			if [ -z ${REPLY} -o ${REPLY} = "q" -o ${REPLY} = "quit" ]; then option="quit"; fi;
-			case ${option} in
-				"quit")
-					echo "you have selected ${srcdirs[@]}"
-					echo "is this correct? (y/n)"
-					read reply
-					if [ "${reply}" = "y" -o -z ${reply} ]; then
-						break
-					fi
-					echo "selection canceled"
-					exit 1
-					;;
-				*)
-					echo "appended ${option} to source directories"
-					srcdirs=(${srcdirs[@]} ${option})
-					;;
-			esac
-		done
-		echo "srcdirs=(`echo ${srcdirs[@]} | sed -E 's/$/"/; s/^/"/; s/ /" "/'`)" >> ${savefile}
-	fi
-}
-get_incdirs() {
-	if [ -z ${incdirs} ]; then
-		echo "
-which of the following directories contain your header files? if header files are in nested directories, please submit all relevant directories"
-		select option in "${directories[@]:1}"
-		do
-			if [ -z ${REPLY} -o ${REPLY} = "q" -o ${REPLY} = "quit" ]; then option="quit"; fi;
-			case ${option} in
-				"quit")
-					echo "you have selected ${incdirs[@]}"
-					echo "is this correct? (y/n)"
-					read reply
-					if [ "${reply}" = "y" -o -z ${reply} ]; then
-						break
-					fi
-					echo "selection canceled"
-					exit 1
-					;;
-				*)
-					echo "appended ${option} to header directories"
-					incdirs=(${incdirs[@]} ${option})
-					;;
-			esac
-		done
-		echo "incdirs=(`echo ${incdirs[@]} | sed -E 's/$/"/; s/^/"/; s/ /" "/'`)" >> ${savefile}
-	fi
-}
-update_makefile() {
-	if [ -e 'makefile' ]; then
-		mv makefile makefile.bk
-	fi
-	echo "${maker_header}
+ensure_directories
 
-SRC = ${srcfiles}
-SRCDIR = ${srcdirs[@]}"'
+# ask for user input on everything not provided by previous runs
+if [ -z ${srcdirs} ]; then get_srcdirs; fi
+if [ -z ${incdirs} ]; then get_incdirs; fi
 
-BIN = $(addprefix $(BINDIR),$(SRC:%.c=%.o))'"
-BINDIR = ${bindir}
-" > makefile
-	if [ ! -z ${libdir} ]; then 
-		echo "LIBDIR = ${libdir}
-LIBRARIES = `find ${libdir} -mindepth 1 -type d -printf "%P/%P.a "`"'
-LIB = $(addprefix $(LIBDIR),$(LIBRARIES))
-' >> makefile
-	fi
-	echo "INCDIR = ${incdirs[@]}"'
-INC = $(addprefix -I,$(INCDIR) .)'"
-
-DEPDIR = ${depdir}"'
-DEPFLAGS = -MM -MF $@ -MT $@ -MT $(BINDIR)$(addsuffix .o,$(notdir $(basename $<)))
-' >> makefile
-	if [ ${language} = "c" ]; then
-		echo "CC = cc
-CFLAGS = ${flags}"' $(INC)
-' >> makefile
-	else
-		echo "CXX = c++
-CXXFLAGS = ${flags}"' $(INC)
-' >> makefile
-	fi
-	echo "NAME = ${targetbin}"'
-VPATH = $(SRCDIR)
-.DEFAULT_GOAL = $(NAME)
-.PHONY : all clean fclean re
-.PRECIOUS : $(BINDIR) $(DEPDIR)
-
-include $(addprefix $(DEPDIR), $(SRC:%.c=%.d))
-
-define libscmd
-for dir in $(dir $(LIB)); do \
-	$(MAKE) -C $$dir $(1); done ;
-endef
-
-$(LIB) :
-	+$(MAKE) -C $(dir $@)
-
-# Default commands
-all : $(NAME)
-clean :
-	$(RM) -r $(BINDIR)
-	+$(call libscmd, clean)
-fclean :
-	$(RM) -r $(BINDIR) $(DEPDIR)
-	$(RM) $(NAME)
-	+$(call libscmd, fclean)
-re :
-	+$(MAKE) fclean
-	+$(MAKE)
-
-%/ :
-	mkdir -p $@
-
-$(DEPDIR)%.d : %.'"${language}"' | $(DEPDIR)
-	$('`if [ ${language} = "c" ];then echo "CC"; else echo "CXX"; fi`') $('`if [ ${language} = "c" ];then echo "CFLAGS"; else echo "CXXFLAGS"; fi`') $(CPPFLAGS) $(DEPFLAGS) $<
-
-$(BINDIR)%.o : %.'"${language}"' | $(BINDIR)
-	$('`if [ ${language} = "c" ];then echo "CC"; else echo "CXX"; fi`') $('`if [ ${language} = "c" ];then echo "CFLAGS"; else echo "CXXFLAGS"; fi`') $(CPPFLAGS) -c -o $@ $<
-
-# Extra commands
-run : $(NAME); ./$(NAME)
-debug : CFLAGS+=-g3
-debug : CFLAGS+=-g3
-debug : re
-
-# bread and butter
-$(NAME) : $(BIN) $(LIB)' >> makefile
-	if [ ${language} = "c" ]; then
-		echo '	$(CC) $(CFLAGS) $(INC) -o $@ $^' >> makefile
-	else
-		echo '	$(CXX) $(CXXFLAGS) $(INC) -o $@ $^' >> makefile
-	fi
-}
-
-get_srcdirs
-get_incdirs
-new_srcfiles=`find ${srcdirs[*]} -maxdepth 1 -type f -name "*.${language}" -printf "%f " | sed -E 's/ $//g'`
-if [ ! "${new_srcfiles}" = "${srcfiles}" ]; then
-	sed -i'' '/srcfiles.*/d' ${savefile}
-	echo "srcfiles=\"${new_srcfiles}\"" >> ${savefile}
-	echo "srcfiles changed to: ${new_srcfiles}"
-	srcfiles="${new_srcfiles}"
-	update_makefile
-else
-	echo "no change to srcfiles"
-fi
+update_srcfiles
+update_makefile
